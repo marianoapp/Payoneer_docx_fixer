c516b47103bd099c8a699f3de59104906d624d57
Mariano Appendino <marianoapp@gmail.com>
2021-04-03 20:32:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Payoneer docx fixer
+===================
+
+Fix Word documents so they can be uploaded as attachments in Payoneer.
+
+## Requirements
+Powershell 5 or newer
+
+## Usage
+fix_docx.ps1 FILE
+
+Where FILE is the Word document to fix. A new document with the same name and the suffix "_fixed" will be created.
---
+++ fix_docx.ps1
@@ -0,0 +1,100 @@
+function arraysMatch {
+    param (
+        $firstArray,
+        $secondArray,
+        $startIndex
+    )
+
+    for ($i=0; $i -lt $secondArray.length; $i++) {
+        if ($firstArray[$startIndex+$i] -ne $secondArray[$i]) {
+            return $false
+        }
+    }
+    return $true
+}
+
+$needsRecompression = $false
+
+# validate that an argument was provided
+if ($args.count -ne 1) {
+    write-host "Usage: fix_docx.ps1 file"
+    exit(1)
+}
+
+$filePath = $args[0]
+$file = get-item $filePath
+$newFile = "$($file.basename)_fixed.docx"
+
+# check if a file with the same name as the fixed one already exists
+if (test-path $newFile) {
+    write-host "A file named '$newFile' already exists, aborting"
+    exit(1)
+}
+
+# check the file extension
+if ($file.extension -ne ".docx") {
+    write-host "Wrong file type, it should be a Word document (.docx)"
+    exit(1)
+}
+
+# expected file header
+# 4 bytes magic number
+# 2 bytes minimum version to decompress (we don't care)
+# 2 bytes general purpose bit flags
+$headerSize = 8
+
+# read the signature bytes
+$fs = [IO.File]::OpenRead($file)
+$buffer = [byte[]]::new($headerSize)
+$fs.Read($buffer, 0, $buffer.length) | out-null
+$fs.Close()
+
+# validate the header (magic number)
+$magicNumber = @(0x50, 0x4b, 0x03, 0x04)
+if (!(arraysMatch $buffer $magicNumber 0)) {
+    write-host "This is not a valid file"
+    exit(1)
+}
+
+# validate the flags
+$fixedFlags = @(0x06, 0x00)
+if (arraysMatch $buffer $fixedFlags 6) {
+    write-host "This file is already valid"
+    exit(0)
+}
+
+$emptyFlags = @(0x00, 0x00)
+if (!(arraysMatch $buffer $emptyFlags 6)) {
+    write-host "This file has some flags set, needs to be recompressed"
+    $needsRecompression = $true
+}
+
+# check if the file needs to be recompressed
+if ($needsRecompression) {
+    $tempFile = "$($file.basename).zip"
+    $tempFolder = "./$($file.basename)_temp"
+    
+    # create a file with the .zip extension (expand-archive doesn't work otherwise)
+    copy-item $file $tempFile
+    # decompress the file
+    expand-archive $tempFile $tempFolder
+    # compress it again
+    compress-archive "$tempFolder/*" $tempFile -force
+    # rename the new file
+    move-item $tempFile $newFile
+    # delete the temp folder
+    remove-item -recurse $tempFolder
+}
+else {
+    copy-item $file $newFile
+}
+
+# write the correct flags to the header
+$fs = [IO.File]::OpenWrite((get-item $newFile))
+$fs.Seek(6, "Begin") | out-null
+$fs.WriteByte(0x06)
+$fs.WriteByte(0x00)
+$fs.Flush()
+$fs.Close()
+
+write-host "File fixed"
